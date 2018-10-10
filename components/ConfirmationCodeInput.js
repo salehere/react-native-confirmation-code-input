bafc6e9398cea670c422b4c1cd321e63b8e24dbe
iherem <iherem@iherem2018.local>
2018-10-10 17:00:13 +0700

    fix bug input when key native equal backspace

--- components/ConfirmationCodeInput.js
+++ components/ConfirmationCodeInput.js
@@ -197,9 +197,12 @@ export default class ConfirmationCodeInput extends Component {
   
   _onKeyPress(e) {
     if (e.nativeEvent.key === 'Backspace') {
+      if (Math.abs(this.lastKeyEventTimestamp - e.timeStamp) < 20) return;
       const { currentIndex } = this.state;
       const nextIndex = currentIndex > 0 ? currentIndex - 1 : 0;
       this._setFocus(nextIndex);
+    } else {
+      this.lastKeyEventTimestamp = e.timeStamp;
     }
   }
   
